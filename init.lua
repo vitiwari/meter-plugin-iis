999df74fac42555eee243a54f67075fd9500c122
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-07-14 22:53:33 -0300

    Added copyright
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -1,5 +1,16 @@
--- [boundary.com] IIS Plugin using performance counters
--- [author] Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
+-- Copyright 2015 Boundary, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
 local boundary = require('boundary')
 local Emitter = require('core').Emitter
@@ -175,3 +186,4 @@ function plugin:onParseCommandOutput(output)
 
 end
 plugin:poll()
+
