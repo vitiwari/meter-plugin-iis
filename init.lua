98f05bddce42132ad6fa2d45f9c1c0d8d70f8e98
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-07-16 17:08:56 -0300

    Fixed values returned from performance counters.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -21,26 +21,26 @@ local gsplit = framework.string.gsplit
 local params = framework.params
 
 local metrics_map = {
-	['processor(_total)\\% processor time'] = 'IIS_GENERAL_CPU_USAGE',
-	['system\\processor queue length'] = 'IIS_GENERAL_CPU_QUEUE_LENGTH',
-	['memory\\available bytes'] = 'IIS_GENERAL_MEMORY_FREE',
-	['memory\\pages/sec'] = 'IIS_GENERAL_MEMORY_PAGE_PER_SECOND',
-	['physicaldisk(_total)\\% disk time'] = 'IIS_GENERAL_DISK_TIME',
-	['asp.net applications(__total__)\\requests/sec'] = 'IIS_ASPNET_REQUESTS_PER_SECOND',
-	['asp.net\\application restarts'] = 'IIS_ASPNET_RESTARTS',
-	['asp.net\\request wait time'] = 'IIS_ASPNET_REQUEST_WAIT_TIME',
-	['asp.net\\requests queued'] = 'IIS_ASPNET_REQUESTS_QUEUED',
-	['.net clr exceptions(_global_)\\# of exceps thrown / sec'] = 'IIS_ASPNET_EXECPTIONS_THROWN_PER_SECOND',
-	['.net clr memory(_global_)\\# total committed bytes'] = 'IIS_ASPNET_TOTAL_COMMITTED_BYTES',
-	['web service(_total)\\get requests/sec'] = 'IIS_SERVICE_GET_REQUESTS_PER_SECOND',
-	['web service(_total)\\post requests/sec'] = 'IIS_SERVICE_POST_REQUESTS_PER_SECOND',
-	['web service(_total)\\current connections'] = 'IIS_SERVICE_CURRENT_CONNECTIONS'
+	['processor(_total)\\% processor time'] = {'IIS_GENERAL_CPU_USAGE', 0.01},
+	['system\\processor queue length'] = {'IIS_GENERAL_CPU_QUEUE_LENGTH', 1},
+	['memory\\available bytes'] = {'IIS_GENERAL_MEMORY_FREE', 1},
+	['memory\\pages/sec'] = {'IIS_GENERAL_MEMORY_PAGE_PER_SECOND', 1},
+	['physicaldisk(_total)\\% disk time'] = {'IIS_GENERAL_DISK_TIME', 0.01},
+	['asp.net applications(__total__)\\requests/sec'] = {'IIS_ASPNET_REQUESTS_PER_SECOND', 1},
+	['asp.net\\application restarts'] = {'IIS_ASPNET_RESTARTS', 1},
+	['asp.net\\request wait time'] = {'IIS_ASPNET_REQUEST_WAIT_TIME', 1},
+	['asp.net\\requests queued'] = {'IIS_ASPNET_REQUESTS_QUEUED', 1},
+	['.net clr exceptions(_global_)\\# of exceps thrown / sec'] = {'IIS_ASPNET_EXECPTIONS_THROWN_PER_SECOND', 1},
+	['.net clr memory(_global_)\\# total committed bytes'] = {'IIS_ASPNET_TOTAL_COMMITTED_BYTES', 1},
+	['web service(_total)\\get requests/sec'] = {'IIS_SERVICE_GET_REQUESTS_PER_SECOND', 1},
+	['web service(_total)\\post requests/sec'] = {'IIS_SERVICE_POST_REQUESTS_PER_SECOND', 1},
+	['web service(_total)\\current connections'] = {'IIS_SERVICE_CURRENT_CONNECTIONS', 1}
 }
 
 local cmd = {
 	path = "get_metrics_native.exe",
 	args = {},
-	use_popen = false
+	use_popen = false,
 }
 
 local ds = CommandOutputDataSource:new(cmd)
@@ -50,13 +50,13 @@ local plugin = Plugin:new(params, ds)
 function plugin:onParseValues(data)
 	local result = {}
 	local output = data.output
-	p(data.output)
 	for v in gsplit(output, '\r\n') do
 		local metric, value = v:match('(.+):([%d.?]+)')
 		if metric and value then
-			local boundary_metric = metrics_map[metric]
+			local boundary_metric = metrics_map[metric][1]
+      local factor = metrics_map[metric][2]
 			if boundary_metric then
-				result[boundary_metric] = tonumber(value)
+				result[boundary_metric] = tonumber(value) * factor
 			end
 		end
 	end
